4a56784141ce185e7f5970708250a402b5cce02c
Benjamin Ramser <legionaerr@googlemail.com>
2022-06-22 09:53:24 +0200

    chore(kafka-broker): update kafka dev environment

--- packages/kafka-broker/jest.setup.js
+++ packages/kafka-broker/jest.setup.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-    process.env.KAFKA_BROKER = process.env.KAFKA_BROKER || '192.168.0.10:9092';
+    process.env.KAFKA_BROKER = process.env.KAFKA_BROKER || 'localhost:9092';
     process.env.KAFKAJS_LOG_LEVEL = 'nothing';
-    process.env.SCHEMA_REGISTRY_HOST = 'http://localhost:8081'
+    process.env.SCHEMA_REGISTRY_HOST = 'http://localhost:8081';
 };
