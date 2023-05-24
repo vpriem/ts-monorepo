14f16241e78192c0e77e544ea03acfc91171388d
vpriem <vinzent.priem@gmail.com>
2023-05-24 14:27:55 +0200

    chore(kafka-broker): update confluentinc images to v7.4.0

--- packages/kafka-broker/jest.setup.js
+++ packages/kafka-broker/jest.setup.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-    process.env.KAFKA_BROKER = 'localhost:9092';
+    process.env.KAFKA_BROKER = '127.0.0.1:9092';
     process.env.KAFKAJS_LOG_LEVEL = 'nothing';
-    process.env.SCHEMA_REGISTRY_HOST = 'http://localhost:8081';
+    process.env.SCHEMA_REGISTRY_HOST = 'http://127.0.0.1:8081';
 };
